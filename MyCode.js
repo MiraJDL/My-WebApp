dc6fa54ccf7c466ab5c2ccb7c1384efa565201d2
MiraJDL <32495595+MiraJDL@users.noreply.github.com>
2017-12-03 17:55:52 -0800

    added scenarios. buttons don't disappear

--- MyCode.js
+++ MyCode.js
@@ -60,7 +60,7 @@ function home(){
   canvas = createCanvas(windowWidth, 1200);
   canvas.position(0, 82);
   canvas.style('z-index', '-2');
-  background(96, 154, 160);
+  background(113, 178, 186);
   intro = "To understand the psycological effects of virtual reality (VR), we have to know what VR is, and how it's used."
   fill(0);
   strokeWeight(0);
@@ -80,8 +80,11 @@ function home(){
       text(whatPar2, 10, 205, windowWidth*0.95);
       strokeWeight(0);
       dreamPar = "VR feels a bit like how a dream feels. It feels like it's actually happening, even though it's (potencially) unrealistic."
-      fill(0);
+      fill(31, 51, 85);
       text(dreamPar, windowWidth*0.6+170, 110, 250);
+      noFill();
+      strokeWeight(2);
+      rect(windowWidth*.6+167, 95, 250, 75);
 
   //how
     howTitle = "How is VR Used?"
@@ -89,24 +92,28 @@ function home(){
     textSize(20);
     text(howTitle, 10, 330);
       textSize(17);
+      fill(58, 2, 2);
       entertainmentTitle = howSections[0];
       text(entertainmentTitle, 100, 350);
         entPar = "VR is most commonly used in the entertainment industry. VR is used for movies, museums, and travel, but probably most famously for video games. Virtual reality gives the viewer a more realistic experience, which allows them to be more engaged in the environment."
         textSize(14);
         text(entPar, 50, 367, 250);
       textSize(17);
+      fill(58, 2, 58);
       healthTitle = howSections[1]
       text(healthTitle, 380, 350);
         healthPar = "Virtual reality is also being used in the healthcare industry. It’s used as a risk free way to train surgeons. It can sometimes be used as a diagnostic and could replace surgery. However, what is the most intriguing, is that VR is used as a type of therapy. It has been proven to help patients with PTSD, and Paranoia."
         textSize(14);
         text(healthPar, 320, 367, 250);
       textSize(17);
+      fill(1, 5, 38);
       NASATitle = howSections[2]
       text(NASATitle, 660, 350);
         NASAPar = "NASA uses VR to control their robots on Mars, which is billions of miles away. They also use it to train astronauts and to give people the experience of walking through space."
         textSize(14);
         text(NASAPar, 600, 367, 250);
       textSize(17);
+      fill(1, 43, 3);
       militaryTitle = howSections[3]
       text(militaryTitle, 920, 350);
         militaryPar = "The Military uses VR to train soldiers. They use it to simulate flight, for medic training in the battlefield, vehicle simulation, as well as other things."
@@ -117,6 +124,7 @@ function home(){
   //psycology
     psycologyTitle = "Is VR Psycologically Unhealthy?"
     textSize(20);
+    fill(0);
     text(psycologyTitle, 10, 600);
       psycologyIntro = "What are the psychological effects of virtual reality? Check out the different scenerios below to find out."
       textSize(14);
@@ -126,19 +134,15 @@ function home(){
       psycologyPar = "Because virtual reality feels so real, it triggers much more genuine emotions, which is not always a good thing. When are these psychological effects a bad thing? Well you explored the answer to that when you looked at the different scenarios. So back to our original question: is virtual reality psychologically unhealthy? It depends on what it’s used for. It can be a great thing for people suffering from various mental health issues. It also helps with science and the military, as well as various creative industries. However, if used in some types of entertainment (as you saw in the scenarios above) it can be quite destructive."
       text(psycologyPar, 50, 1090, 1000);
 
-    //bback.hide();
-  //  img1 = createImage(250, 167);
-  //  //image(img1, 200, 300);
-
 }//end home end home end home end home end home end home
 
 
 function buttonWorks(){
   //senarios
   movie = basicbutton(windowWidth*0.1, 730, "movie/video game", movies);
-  travel = basicbutton(windowWidth*0.35, 730, "travel", movies);
-  science = basicbutton(windowWidth*0.55, 730, "science", movies);
-  therapy = basicbutton(windowWidth*0.75, 730, "therapy", movies);
+  travel = basicbutton(windowWidth*0.35, 730, "travel", travel);
+  science = basicbutton(windowWidth*0.55, 730, "science", science);
+  therapy = basicbutton(windowWidth*0.75, 730, "therapy", therapy);
   //images
   puppyBut = basicbutton(windowWidth*0.2, 820, "click me", showPuppyStuff);
   scaryBut = basicbutton(windowWidth*0.7, 820, "click me", showScaryStuff);
@@ -158,22 +162,78 @@ function movies(){
   DarkCanvas();
   darken = true;
   fill(200);
-  canvas1 = rect(120, 0,  windowWidth*0.8, 1400);
+  canvas1 = rect(120, 0,  windowWidth*0.8, 600);
   var par1 = "You’re super hyped about a new version of a video game titled ‘The Evil Within’. As an early Christmas present your cousin Willie gave you the new Samsung Odyssey VR headset. You’ve played ‘The Evil Within’ before, but not with VR."
   noStroke();
-  fill(0);
+  fill(99, 10, 28);
   text(par1, 150, 60, 400);
   var par2 = "You get the game to test it out...and it is scary. I mean you actually screamed out loud seven times, and before that the most you screamed was maybe twice. You come away from it shaken."
   text(par2, 550, 200, 400);
-  var par3 = "As you go about your daily life, you start to become more agitated. You keep thinking about that video game, and all those jump scares. You become anxious and fearful, and you lose interest in things you once found entertaining. Not to mention you completely abandon playing ‘The Evil Within’, or any scary video game for that matter."
+  var par3 = "As you go about your daily life, you start to become more agitated. You keep thinking about that video game, and all those jump scares. You become anxious and fearful, and you lose interest in things you once found entertaining. Not to mention you completely abandon playing ‘The Evil Within’, or any scary video game for that matter. You’re suffering from post traumatic stress disorder."
   text(par3, 150, 300, 400);
-  var par4 = "You’re suffering from post traumatic stress disorder."
-  textSize(17);
-  text(par4, 150, 440);
+  var par4 = "Scroll up"
+  text(par4, 150, 590);
   bback = basicbutton(10, 10, 'back', back);
   bback.style('z-index', '1');
 }//end movies
 
+
+function travel(){
+  DarkCanvas();
+  darken = true;
+  fill(200);
+  canvas1 = rect(120, 0,  windowWidth*0.8, 600);
+  var par1 = "You work as a businessman for a company that you’ve put a lot of time and effort into. They recently ordered a now shipment of VR equipment so that you can virtually travel to meetings, instead of actually going. The company has gifted you with your own headset. You find this new technology very fascinating and you start spending a lot of time discovering its different features."
+  noStroke();
+  fill(14, 72, 112);
+  text(par1, 150, 60, 400);
+  var par2 = "You find that you can not only travel to meetings, but to other places, like Hawaii or Paris. Because of the time this technology saves, your boss has dumped a bunch of extra work for you to do. This causes much stress, and you find it relaxing to take a break and virtually visit the Arc de Triomphe."
+  text(par2, 550, 220, 400);
+  var par3 = "You spend more and more time traveling to various places all over the world, so much so that you get behind on work. You start becoming detached from your friends, family, and most other parts of your life as your addiction becomes worse and worse."
+  text(par3, 150, 340, 400);
+  var par4 = "Scroll up"
+  text(par4, 150, 590);
+  bback = basicbutton(10, 10, 'back', back);
+  bback.style('z-index', '1');
+}
+
+function science(){
+  DarkCanvas();
+  darken = true;
+  fill(200);
+  canvas1 = rect(120, 0,  windowWidth*0.8, 600);
+  var par1 = "You just got a degree in science and are eager to head into the science field. You want to introduce VR to the field and use it to further investigate how things work. You get a job and are successful in proposing this idea to your bosses."
+  noStroke();
+  fill(19, 130, 63);
+  text(par1, 150, 60, 400);
+  var par2 = "You help your community implicate the VR technology into their work. It takes a lot of effort, but it pays off. Thanks to the VR, people, including yourself are making new discoveries and are able to approach problems in a way that previously wasn’t possible."
+  text(par2, 550, 205, 400);
+  var par3 = "Your work resulted in numerous scientific breakthroughs, and you got promoted. Your self-confidence is high, and you are more than comfortable with your situation."
+  text(par3, 150, 340, 400);
+  var par4 = "Scroll up"
+  text(par4, 150, 590);
+  bback = basicbutton(10, 10, 'back', back);
+  bback.style('z-index', '1');
+}
+
+function therapy(){
+  DarkCanvas();
+  darken = true;
+  fill(200);
+  canvas1 = rect(120, 0,  windowWidth*0.8, 600);
+  var par1 = "You are a patient who suffers from paranoia. You have been visiting your therapist for a few years now, and it has definitely helped you. However, you are still not satisfied by the rate at which you are getting better. One day your therapist tells you about how VR technology is being used to help people like you, and suggests you try it."
+  noStroke();
+  fill(99, 17, 117);
+  text(par1, 150, 60, 400);
+  var par2 = "You take their advice and test it out. You use VR to enter a situation where you would be very uncomfortable (in a crowded elevator, or on a crowded subway), but since it’s VR you know you are safe. This helps you and you return to try it again."
+  text(par2, 550, 205, 400);
+  var par3 = "After some time of using VR, you are able to ride the train without feeling as uncomfortable. You become less anxious, and begin to overcome your paranioa. You become a more relaxed and happy person."
+  text(par3, 150, 340, 400);
+  var par4 = "Scroll up"
+  text(par4, 150, 590);
+  bback = basicbutton(10, 10, 'back', back);
+  bback.style('z-index', '1');
+}
 
 function DarkCanvas(){
   fill(0, 200);
